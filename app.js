1f53428105809294539b0576e50ea413eff56b17
Dadidam <joker.pnz@gmail.com>
2017-08-29 14:19:41 -0400

    Write ng-controller

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-var app = angular.module('store', [ ]);
+(function() {
+  var app = angular.module('store', [ ]);
+
+  app.controller('StoreController', function() {
+    this.product = gem;
+  });
+
+  var gem = {
+    name: 'Dodic',
+    price: 2.95,
+    description: 'Some text here'
+  }
+})();
