0c2f98b044f0fe23eab8a55297859746ebfc5635
Dadidam <joker.pnz@gmail.com>
2017-08-30 16:50:09 -0400

    Templating with Custom Directives, Smarter Custom Directives

--- app.js
+++ app.js
@@ -32,12 +32,30 @@
   /*
     Custom Directions
   */
-
   app.directive('productTitle', function() {
     return {
       restrict: 'A',
       templateUrl: 'product-title.html'
     };
+  });
+
+  app.directive('productPanels', function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'product-panels.html',
+      controller: function() {
+        this.tab = 1;
+    
+        this.selectTab = function(setTab) {
+          this.tab = setTab;
+        };
+
+        this.isSelected = function(checkTab) {
+          return this.tab === checkTab;
+        };
+      },
+      controllerAs: 'panel'
+    }
   });
 
   var gems = [
